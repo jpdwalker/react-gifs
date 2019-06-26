67a926c5ed5df680a38b137ffc9d8df3d629781a
Joe Walker <jpdwalker@gmail.com>
2019-06-26 15:13:52 +0800

    completed react gifs app challenge

---
+++ src/components/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "jRhz5BLxXFYTS"
+    };
+  }
+
+  search = (query) => {
+    giphy('SJI98mA8ENdrYt3Xkc8md4eoacwiVVoc').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="left-scene">
+            <SearchBar search={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
